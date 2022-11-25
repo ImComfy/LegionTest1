ebde515d6de97ef4b6d493c38c299fecde1e6567
ImComfy <relxdspeedrunanypercentage@gmail.com>
2022-11-25 20:59:38 +0100

    added stuff

--- src/main/java/me/comfy/legiontest1/commands/NickCommand.java
+++ src/main/java/me/comfy/legiontest1/commands/NickCommand.java
@@ -5,7 +5,10 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+
+import static org.bukkit.Bukkit.getLogger;
 
 public class NickCommand implements CommandExecutor {
     @Override
@@ -15,16 +18,19 @@ public class NickCommand implements CommandExecutor {
             if (p.isOp() || p.hasPermission("legiontest.nick")){
                 if (args.length == 0 || args.length == 1){
                     p.sendMessage("/nick set (player) (nick)");
-                    p.sendMessage("/nick random (player)");
                     p.sendMessage("/nick (player)");
                 }else if (args.length == 2){
                     if (args[0].equalsIgnoreCase("set")){
-                        Player target = Bukkit.getPlayer(args[0]);
-                        String nickname = args[1];
-                        target.setDisplayName(nickname);
-                        target.setPlayerListName(nickname);
-                        p.sendMessage(ChatColor.GREEN + "Successfully nicked " + target.getName() + " to " + nickname);
-                        target.sendMessage(ChatColor.GREEN + "Nickname has been set to " + "\"" + nickname + "\"" + " by " + p.getDisplayName());
+                        try {
+                            Player target = Bukkit.getPlayer(args[0]);
+                            String nickname = args[1];
+                            target.setDisplayName(nickname);
+                            target.setPlayerListName(nickname);
+                            p.sendMessage(ChatColor.GREEN + "Successfully nicked " + target.getName() + " to " + nickname);
+                            target.sendMessage(ChatColor.GREEN + "Nickname has been set to " + "\"" + nickname + "\"" + " by " + p.getDisplayName());
+                        }catch (NullPointerException n){
+                            p.sendMessage(ChatColor.RED + "That player is not online.");
+                        }
                     }
                 }
 
@@ -34,12 +40,33 @@ public class NickCommand implements CommandExecutor {
                         p.setDisplayName(name);
                         p.setPlayerListName(name);
                     }else if (args.length == 1){
-                        Player target = Bukkit.getPlayer(args[0]);
-                        String name = target.getName();
-                        target.setDisplayName(name);
-                        target.setPlayerListName(name);
-                        p.sendMessage(ChatColor.GREEN + "Successfully removed nick from " + target.getName());
-                        target.sendMessage(ChatColor.GREEN + "Nickname removed by " + p.getDisplayName());
+                        try {
+                            Player target = Bukkit.getPlayer(args[0]);
+                            String name = target.getName();
+                            target.setDisplayName(name);
+                            target.setPlayerListName(name);
+                            p.sendMessage(ChatColor.GREEN + "Successfully removed nick from " + target.getName());
+                            target.sendMessage(ChatColor.GREEN + "Nickname removed by " + p.getDisplayName());
+                        }catch (NullPointerException n){
+                            p.sendMessage(ChatColor.RED + "That player is not online.");
+                        }
+                    }
+                }
+            }
+        }else if (sender instanceof ConsoleCommandSender console){
+            if (args.length == 0 || args.length == 1){
+                getLogger().info("/nick set (player) (nick)");
+            }else if (args.length == 2){
+                if (args[0].equalsIgnoreCase("set")){
+                    try {
+                        Player target = Bukkit.getPlayer(args[1]);
+                        String nickname = args[2];
+                        target.setDisplayName(nickname);
+                        target.setPlayerListName(nickname);
+                        getLogger().info("\"Successfully nicked \" + target.getName() + \" to \" + nickname");
+                        target.sendMessage(ChatColor.GREEN + "Nickname has been set to " + "\"" + nickname + "\"" + " by " + ChatColor.AQUA + "@System");
+                    }catch (NullPointerException n){
+                        getLogger().info("That player is not online.");
                     }
                 }
             }
